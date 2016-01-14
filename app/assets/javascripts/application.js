2bb0df3ae747519a823c0864bc29bc715f5f33bc
John Efthymiou <johnnyeft@gmail.com>
2016-01-15 00:24:09 +0200

    Added Best In Place. There's a but in announcement editing, sometimes the message doesn't appear after trying to edit the title

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery.purr
+//= require best_in_place
 //= require_tree .
--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,7 @@
 class AnnouncementsController < ApplicationController
+  before_action :set_course, only: [:create, :update]
+  before_action :set_announcement, only: [:update]
   def create
-    @course = Course.find(params[:course_id])
     @announcement = @course.announcements.new(announcement_params)
     @announcement.user_id = current_user.id
 
@@ -17,8 +18,28 @@ class AnnouncementsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @announcement.update(announcement_params)
+        format.html { redirect_to @course, notice: 'Announcement was successfully updated.' }
+        format.json { respond_with_bip(@announcement) }
+      else
+        format.html { render :edit }
+        format.json { render json: @announcement.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def announcement_params
       params.require(:announcement).permit(:title, :message)
+    end
+
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
+    def set_announcement
+      @announcement = Announcement.find(params[:id])
     end
 end
